12a81cbd8a3c0729cad698b5b2a840820aa88d17
13308 <varun.bondada@gmail.com>
2016-09-16 18:00:27 +0530

    add open silding and links

--- src/components/overlay-slider.js
+++ src/components/overlay-slider.js
@@ -18,14 +18,20 @@ export default class OverlaySlider extends React.Component {
 
 	componentDidMount() {
 		this.timeline = new TimelineLite({ paused: true });
-
-		console.log(this.refs.img1);
+		
+		// console.log(this.refs.img1);
 		this.timeline
-			.to(this.refs.overlay, 0.5, { x: '-95%', ease: Power1.easeInOut, y: 0 })
+			.to(this.refs.overlay, 0.5,{ x: '-94%', ease: Power1.easeInOut, y: 0 })
 			.from(this.refs.img0, 0.5, { y: '100%' }, '-=0.25')
 			.from(this.refs.img1, 0.5, { y: '100%' }, '-=0.25')
-			.from(this.refs.img2, 0.5, { y: '100%' }, '-=0.25');
+			.from(this.refs.img2, 0.5, { y: '100%' }, '-=0.25')
+			.from(this.refs.openArrow, 1, {x:+2,rotation: '+=180', ease:  Back.easeOut.config(1)}, '-=1')
+			.to(this.refs.openArrow, 1, {x:+3,rotation: '+=180', ease:  Back.easeOut.config(1)}, '-=1');
 	}
+
+
+
+
 
 	animateStyle(){
 		let style={
@@ -34,55 +40,49 @@ export default class OverlaySlider extends React.Component {
 			background:'hsla(0,0%,0%,0.7)',
 			position:'absolute',
 			top: 0,
-			left: '95%'
+			left: '94%'
 		}
 		return style;
 	}
 
-	clickmaster(){
-		console.log(this.refs.overlay);
+	clickmaster(e){
+	console.log(e.target.closest('a'));
 
-		if (this.state.open) {
-			this.setState({
-				open: false
-			});
-			this.timeline.reverse();
-		} else {
-			this.setState({
-				open: true
-			});
-			this.timeline.play();
+		if (e.target.closest('a')) {
+
+				this.setState({
+					open: true
+				});
+				this.timeline.play();
+		}
+	
+		else{
+			if (this.state.open) {
+				this.setState({
+					open: false
+				});
+				this.timeline.reverse();
+			} else {
+				this.setState({
+					open: true
+				});
+				this.timeline.play();
+			}
 		}
 	}
 
-	// overlayButtonStyle(){
-	// 	let style = {
-	// 		fontSize: this.props.data.overlayButtonStyle.FontSize,
-	// 		backgroundColor: this.props.data.overlayButtonStyle.bgColor,
-	// 		color: this.props.data.overlayButtonStyle.color,
-	// 		transition:this.props.data.overlayButtonStyle.trans,
-	// 		position: 'absolute',
-	// 		textAlign: 'center',
-	// 		top: 0,
-	// 		height: '100%',
-	// 		width: '100%',
-	// 	}
 
-
-	// 	if (this.state.open) {
-	// 		_.assignIn(style, {
-	// 			// width: '100%'
-	// 			left: 0
-	// 		})
-	// 	}else{
-	// 		_.assignIn(style, {
-	// 			left: window.innerWidth - 20
-	// 			// width: 20
-	// 		})			
-	// 	}
-
-	// 	return style;
-	// }
+	OverlayContentImageStyle(){
+		let style = {
+			display: 'block',
+			width:'33.3%', 
+			height: 'auto', 
+			float:'left', 
+			padding: '12px 5px', 
+			padding:'32px 5px 0px 19px'
+		}
+		return style;
+	}
 
 	// showOverlayText(){
 	// 	let overlayTex;
@@ -95,82 +95,12 @@ export default class OverlaySlider extends React.Component {
 	// 	return overlayTex;
 	// }
 
-
-	// overlayContentStyle(){
-		// let style = {
-		// 	position: 'absolute',
-		// 	top: 0,
-		// 	left: 0,
-		// 	marginLeft: 20,
-		// 	height: '100%',
-		// 	width: window.innerWidth - 20,
-		// 	zIndex: 9,
-		// 	transition:'all .5s ease-in-out',
-		// }
-
-
-		// if (this.state.open) {
-		// 	_.assignIn(style, {
-		// 		display:'block'
-		// 		// width:window.innerWidth - 20,
-		// 	})
-		// }
-
-
-
-		// return style;
-	// }
-
-	OverlayContentImageStyle(){
-		let style = {
-			display: 'block',
-			width:'33.3%', 
-			height: 'auto', 
-			float:'left', 
-			padding: '12px 5px', 
-			padding:5
-		}
-		return style;
-	}
-
-	// cnotentImageStyle(){
-	// 	let style = {
-	// 		width: '33%', 
-	// 		padding: '14px 3px',
-	// 		height:'100%', 
-	// 		float:'left', 
-	// 		display:'block'
-	// 	}
-	// 	return style;
-	// }
-
-	// ShowOverlayContent(){
-
-	// 	const cnotentImages = this.props.data.overlayItems.map((val, key)=>{
-	// 		return(
-	// 				<a style={this.cnotentImageStyle()} key={key} href={val.shopLink} target="_blank">
-	// 					<div style={{display:'table', height:'100%'}}>
-	// 						<div  style={{display:'table-cell', verticalAlign: 'middle'}}>
-	// 							<img style={this.OverlayContentImageStyle()} src={val.imgURL} alt='Look1' />
-	// 						</div>
-	// 					</div>
-	// 				</a>
-	// 			)
-	// 	});
-	// 	return cnotentImages;
-	// }
-
-	ShowOverlayContent(){
-
-		
-	}
-
 	render() {
-
+// console.log(this.props.data.overlayItems.map(val, key));
 		const cnotentImages = this.props.data.overlayItems.map((val, key)=>{
-					console.log('img'+key);
+					// console.log('img'+key);
 			return(
-					<img ref={'img'+key} key={key} style={this.OverlayContentImageStyle()} src={val.imgURL} />
+					<a key={key} href={val.shopLink} target="_blank"><img ref={'img'+key}  style={this.OverlayContentImageStyle()} src={val.imgURL} /></a>
 				)
 		});
 
@@ -178,22 +108,16 @@ export default class OverlaySlider extends React.Component {
 			<div class="wrap-container">
 				<div style={{position:'relative', overflow: 'hidden'}} class="container">
 					<img onClick={this.clickmaster.bind(this)} style={{display: 'block',width:'100%'}} src= {this.props.data.masterimage} alt="Master Image" />
-					{/*<div style={this.overlayButtonStyle()}>
-						<div onClick={this.clickmaster.bind(this)} style={{display: 'table', height:'100%', zIndex: 99}}>
-							<div style={{width:20, display:'table-cell', verticalAlign: 'middle', fontWeight: 'bold', fontSize: 22, padding: 4}}>
-								{this.showOverlayText()}
-							</div>
-						</div>
-						<div ref="overlay" style={this.overlayContentStyle()}>
-							{this.ShowOverlayContent()}
-						</div>
-					</div>*/}
+		
+					
+
 					<div ref="overlay" onClick={this.clickmaster.bind(this)} style={this.animateStyle()} ref="overlay">
+						<div ref="openArrow" style={{color:'#fff', fontWeight: 'bold', fontSize: 22, padding: '4px 0px' ,top:'40%',position:'absolute',zIndex:1}}>></div>
 						{cnotentImages}
-						{/*<div ref="img1" style={{ display: 'inline-block',width: '33.3%', height: '100%', backgroundColor: 'indigo'}}></div>
-						<div ref="img2" style={{display: 'inline-block', width: '33.3%', height: '100%', backgroundColor: 'green'}}></div>
-						<div ref="img3" style={{display: 'inline-block', width: '33.3%', height: '100%', backgroundColor: 'yellow'}}></div>*/}
+						
 					</div>
+			
+					
 				</div>
 
 			</div>
